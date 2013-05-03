d6291522fa2060d4cb1eba9005a9284fb98713b4
Stéphane Demonchaux <demonchaux.stephane@gmail.com>
2013-05-03 16:19:15 +0200

    Multiple Fix

--- src/CrudGenerator/Generators/DoctrineCrudGenerator.php
+++ src/CrudGenerator/Generators/DoctrineCrudGenerator.php
@@ -16,10 +16,10 @@ class DoctrineCrudGenerator extends BaseCodeGenerator
         $this->skeletonDir = __DIR__ . '/ArchitectGenerator/Skeleton';
 
         //$this->ifDirDoesNotExistCreate($dataObject->getModule() . '');
-        $this->ifDirDoesNotExistCreate($dataObject->getModule() . '/' . $dataObject->getNamespacePath() . '/Form/');
-        $this->ifDirDoesNotExistCreate($dataObject->getModule() . '/' . $dataObject->getNamespacePath() . '/DAO/');
-        $this->ifDirDoesNotExistCreate($dataObject->getModule() . '/' . $dataObject->getNamespacePath() . '/Hydrator/');
-        $this->ifDirDoesNotExistCreate($dataObject->getModule() . '/' . $dataObject->getNamespacePath() . '/DataObject/');
+        $this->ifDirDoesNotExistCreate($dataObject->getModule() . '/' . $dataObject->getDirectory() . '/Form/');
+        $this->ifDirDoesNotExistCreate($dataObject->getModule() . '/' . $dataObject->getDirectory() . '/DAO/');
+        $this->ifDirDoesNotExistCreate($dataObject->getModule() . '/' . $dataObject->getDirectory() . '/Hydrator/');
+        $this->ifDirDoesNotExistCreate($dataObject->getModule() . '/' . $dataObject->getDirectory() . '/DataObject/');
 
 
         /*$this->generateFile($dataObject, '/crud/controller.php.phtml', $dataObject->getControllerPath() . $dataObject->getClassName() . 'Controller.php');
@@ -39,12 +39,12 @@ class DoctrineCrudGenerator extends BaseCodeGenerator
         }*/
 
         //$this->generateFile($dataObject, '/form/FormType.php.phtml', $dataObject->getModule() . '/' . $dataObject->getNamespacePath() . '/Form/' . $dataObject->getEntityName() . 'Form.php');
-        $this->generateFile($dataObject, '/DAOFactory.php.phtml', $dataObject->getModule() . '/' . $dataObject->getNamespacePath() . '/' . $dataObject->getEntityName() . 'DAOFactory.php');
-        $this->generateFile($dataObject, '/DAO.php.phtml', $dataObject->getModule() . '/' . $dataObject->getNamespacePath() . '/DAO/' . $dataObject->getEntityName() . 'DAO.php');
-        $this->generateFile($dataObject, '/Exception.php.phtml', $dataObject->getModule() . '/' . $dataObject->getNamespacePath() . '/No' . $dataObject->getEntityName() . 'Exception.php');
-        $this->generateFile($dataObject, '/Hydrator.php.phtml', $dataObject->getModule() . '/' . $dataObject->getNamespacePath() . '/Hydrator/' . $dataObject->getEntityName() . 'Hydrator.php');
-        $this->generateFile($dataObject, '/DataObjectCollection.php.phtml', $dataObject->getModule() . '/' . $dataObject->getNamespacePath() . '/DataObject/' . $dataObject->getEntityName() . 'Collection.php');
-        $this->generateFile($dataObject, '/DataObject.php.phtml', $dataObject->getModule() . '/' . $dataObject->getNamespacePath() . '/DataObject/' . $dataObject->getEntityName() . 'DataObject.php');
+        $this->generateFile($dataObject, '/DAOFactory.php.phtml', $dataObject->getModule() . '/' . $dataObject->getDirectory() . '/' . $dataObject->getEntityName() . 'DAOFactory.php');
+        $this->generateFile($dataObject, '/DAO.php.phtml', $dataObject->getModule() . '/' . $dataObject->getDirectory() . '/DAO/' . $dataObject->getEntityName() . 'DAO.php');
+        $this->generateFile($dataObject, '/Exception.php.phtml', $dataObject->getModule() . '/' . $dataObject->getDirectory() . '/No' . $dataObject->getEntityName() . 'Exception.php');
+        $this->generateFile($dataObject, '/Hydrator.php.phtml', $dataObject->getModule() . '/' . $dataObject->getDirectory() . '/Hydrator/' . $dataObject->getEntityName() . 'Hydrator.php');
+        $this->generateFile($dataObject, '/DataObjectCollection.php.phtml', $dataObject->getModule() . '/' . $dataObject->getDirectory() . '/DataObject/' . $dataObject->getEntityName() . 'Collection.php');
+        $this->generateFile($dataObject, '/DataObject.php.phtml', $dataObject->getModule() . '/' . $dataObject->getDirectory() . '/DataObject/' . $dataObject->getEntityName() . 'DataObject.php');
         //$this->generateFile($dataObject, '/architect/FormBuilderFactory.php.phtml', $dataObject->getModule() . '/' . $dataObject->getNamespacePath() . '/' . $dataObject->getEntityName() . 'FormBuilderFactory.php');
         //$this->generateFile($dataObject, '/architect/FormBuilder.php.phtml', $dataObject->getModule() . '/' . $dataObject->getNamespacePath() . '/Form/' . $dataObject->getEntityName() . 'FormBuilder.php');
         //$this->_generateFile($dataObject, '/config/routes.ini.php.phtml', APPLICATION_PATH . '/configs/routes.ini');
