07c402013feb7db094e1ac802994329bbeb25d5c
Stéphane Demonchaux <demonchaux.stephane@gmail.com>
2013-05-06 14:54:41 +0200

    Add unit test generation

--- src/CrudGenerator/Generators/ArchitectGenerator/ArchitectGenerator.php
+++ src/CrudGenerator/Generators/ArchitectGenerator/ArchitectGenerator.php
@@ -10,7 +10,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 class ArchitectGenerator extends BaseCodeGenerator
 {
     protected $skeletonDir = null;
-    protected $definition  = 'Generate DAO, DTO, Hydrator, Exception';
+    protected $definition  = 'Generate DAO, DTO, Hydrator, Exception, unit test';
 
     /**
      * @param DataObject $dataObject
@@ -33,5 +33,55 @@ class ArchitectGenerator extends BaseCodeGenerator
         $this->generateFile($dataObject, '/Hydrator.php.phtml', $dataObject->getModule() . '/' . $dataObject->getDirectory() . '/Hydrator/' . $dataObject->getEntityName() . 'Hydrator.php');
         $this->generateFile($dataObject, '/DataObjectCollection.php.phtml', $dataObject->getModule() . '/' . $dataObject->getDirectory() . '/DataObject/' . $dataObject->getEntityName() . 'Collection.php');
         $this->generateFile($dataObject, '/DataObject.php.phtml', $dataObject->getModule() . '/' . $dataObject->getDirectory() . '/DataObject/' . $dataObject->getEntityName() . 'DataObject.php');
+
+        $generateUnitTest = $this->dialog->ask($this->output, 'Would you like to generate unitTest ?');
+
+        if($generateUnitTest != 'y') {
+            return false;
+        }
+
+        $moduleName = str_replace('/', '', strstr($dataObject->getModule(), '/'));
+        $unitTestDirectory = $dataObject->getModule() . '/src/' . $moduleName . '/test/';
+        $this->ifDirDoesNotExistCreate($unitTestDirectory);
+        $this->ifDirDoesNotExistCreate($unitTestDirectory . $moduleName . 'Test');
+        $unitTestDirectory = $unitTestDirectory. $moduleName . 'Test/';
+        $suppdatas = array(
+            'unitTestNamespace' => $moduleName . 'Test'
+        );
+
+        $this->generateFile(
+            $dataObject, '/test/FixtureManager.php.phtml',
+            $unitTestDirectory . 'FixtureManager.php',
+            $suppdatas
+        );
+
+        $modelDirectory = explode('\\', $this->generiqueQuestion->namespaceQuestion());
+        $allDir = '';
+        foreach($modelDirectory as $dir) {
+            $allDir .= '/' . $dir;
+            $this->ifDirDoesNotExistCreate($unitTestDirectory . $allDir);
+        }
+
+        $this->ifDirDoesNotExistCreate($unitTestDirectory . $allDir . '/DAOFactory');
+
+        $this->generateFile(
+            $dataObject, '/test/DAOFactory/getInstanceTest.php.phtml',
+            $unitTestDirectory . $allDir . '/DAOFactory/getInstanceTest.php',
+            $suppdatas
+        );
+
+        $this->ifDirDoesNotExistCreate($unitTestDirectory . $allDir . '/DAO');
+
+        $this->generateFile(
+            $dataObject, '/test/DAO/findTest.php.phtml',
+            $unitTestDirectory . $allDir . '/DAO/findTest.php',
+            $suppdatas
+        );
+
+        $this->generateFile(
+            $dataObject, '/test/Fixture.php.phtml',
+            $unitTestDirectory . $allDir . '/' . $dataObject->getEntityName() . 'Fixture.php',
+            $suppdatas
+        );
     }
 }
---
+++ src/CrudGenerator/Generators/ArchitectGenerator/Skeleton/test/DAO/persistTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace ApplicationTest\Model\Corp\Client\ClientDAO;
+
+use DateTime;
+
+use ApplicationTest\FixtureManager;
+
+use Corp\Client\ClientDAO;
+use Corp\Client\ClientDataObject;
+
+class persistTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCorrectResult()
+    {
+        FixtureManager::purge();
+        $em = FixtureManager::getEm();
+
+        $numAdeli = 'my_numadeli';
+        $logiciel = 'logiciel';
+        $niveau = 'niveau';
+        $clef = 'clef';
+        $nivUtil = 'nivUtil';
+        $nom = 'nom';
+        $prenom = 'prenom';
+        $typeContract = 'typeContract';
+        $version = 'version';
+        $dateFin = new DateTime();
+
+        $dataObject = new ClientDataObject();
+        $dataObject->setClef($clef)
+                   ->setLogiciel($logiciel)
+                   ->setNiveau($niveau)
+                   ->setNivUtil($nivUtil)
+                   ->setNoAdeli($numAdeli)
+                   ->setNom($nom)
+                   ->setPrenom($prenom)
+                   ->setTypeContract($typeContract)
+                   ->setVersion($version)
+                   ->setDateFin($dateFin);
+
+        $clientDAO = new ClientDAO($em);
+        $clientDAO->persist($dataObject);
+
+        $clefXcabEntity = $em->getRepository('\Corp\Client\ClefXcabEntity')->findOneBy(array('noAdeli' => $numAdeli));
+        $clientXcabEntity = $em->getRepository('\Corp\Client\ClientXcabEntity')->findOneBy(array('noAdeli' => $numAdeli));
+        $contratXcab = $em->getRepository('\Corp\Doctrine\Entity\ContratXcab')->findOneBy(array('noAdeli' => $numAdeli));
+
+        $this->assertEquals($clef, $clefXcabEntity->getClef());
+        $this->assertEquals($numAdeli, $clefXcabEntity->getNoAdeli());
+        $this->assertEquals($version, $clefXcabEntity->getVersion());
+
+        $this->assertEquals($logiciel, $clientXcabEntity->getLogiciel());
+        $this->assertEquals($niveau, $clientXcabEntity->getNiveau());
+        $this->assertEquals($nivUtil, $clientXcabEntity->getNivUtil());
+        $this->assertEquals($numAdeli, $clientXcabEntity->getNoAdeli());
+        $this->assertEquals($nom, $clientXcabEntity->getNom());
+        $this->assertEquals($prenom, $clientXcabEntity->getPrenom());
+
+        $this->assertEquals($dateFin, $contratXcab->getDateFin());
+        $this->assertEquals($numAdeli, $contratXcab->getNoAdeli());
+        $this->assertEquals($typeContract, $contratXcab->getTypeContrat());
+    }
+}
