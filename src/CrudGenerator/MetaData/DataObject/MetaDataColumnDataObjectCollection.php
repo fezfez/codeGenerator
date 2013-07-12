f8ba3a678bd30525d41c2f34466aadda0d37412a
Stéphane Demonchaux <demonchaux.stephane@gmail.com>
2013-07-12 15:56:47 +0200

    Delete a use statement who were not used

--- src/CrudGenerator/MetaData/DataObject/MetaDataColumnDataObjectCollection.php
+++ src/CrudGenerator/MetaData/DataObject/MetaDataColumnDataObjectCollection.php
@@ -4,8 +4,6 @@ namespace CrudGenerator\MetaData\DataObject;
 
 use ArrayObject;
 
-use CrudGenerator\MetaData\MetaDataColumnDataObject;
-
 class MetaDataColumnDataObjectCollection extends ArrayObject
 {
 
