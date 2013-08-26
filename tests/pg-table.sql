e7e668f93d06f1020ade99b811a1b80ad0ab5929
Stéphane Demonchaux <demonchaux.stephane@gmail.com>
2013-08-26 12:35:51 +0200

    Add table on pgsql

---
+++ tests/pg-table.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE messages (
+    id integer NOT NULL,
+    title character varying(255),
+    message text,
+    "time" text
+);
+
+CREATE TABLE suivi_news (
+    id integer NOT NULL,
+    dt_creat character varying(255) NOT NULL,
+    nom_log character varying(10) NOT NULL,
+    ver_log character varying(10) NOT NULL,
+    titre_new character varying(50) NOT NULL,
+    url character varying(250) NOT NULL,
+    nivutil character varying(25) DEFAULT NULL::character varying
+);
