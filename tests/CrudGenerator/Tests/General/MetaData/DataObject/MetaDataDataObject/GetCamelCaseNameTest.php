07044ca5dfc1d9183f2c5a4cf0142f37970723eb
Stéphane Demonchaux <demonchaux.stephane@gmail.com>
2013-08-27 17:36:19 +0200

    Add unit test + bug fix

---
+++ tests/CrudGenerator/Tests/General/MetaData/DataObject/MetaDataDataObject/GetCamelCaseNameTest.php
@@ -0,0 +1,29 @@
+<?php
+namespace CrudGenerator\Tests\General\MetaData\DataObject\MetaDataRelationColumnDataObject;
+
+use CrudGenerator\MetaData\PDO\MetadataDataObjectPDO;
+use CrudGenerator\MetaData\DataObject\MetaDataColumnDataObjectCollection;
+use CrudGenerator\MetaData\DataObject\MetaDataRelationDataObjectCollection;
+
+class GetCamelNameTest extends \PHPUnit_Framework_TestCase
+{
+    public function testType()
+    {
+        $sUT = new MetadataDataObjectPDO(
+            new MetaDataColumnDataObjectCollection(),
+            new MetaDataRelationDataObjectCollection()
+        );
+
+        $sUT->setName('many_to_many');
+
+        $this->assertEquals(
+            'ManyToMany',
+            $sUT->getCamelCaseName(true)
+        );
+
+        $this->assertEquals(
+            'manyToMany',
+            $sUT->getCamelCaseName(false)
+        );
+    }
+}
