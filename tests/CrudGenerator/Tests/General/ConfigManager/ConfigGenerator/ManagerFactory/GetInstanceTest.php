4995ea1b5584d7625a4ccaaaba684ca9c2fe2025
Anthony <arhecatonchires@gmail.com>
2013-08-29 15:29:19 +0200

    Configuration engine for read abstracts metadata

---
+++ tests/CrudGenerator/Tests/General/ConfigManager/ConfigGenerator/ManagerFactory/GetInstanceTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace CrudGenerator\Tests\General\ConfigGenerator\ConfigManager\ManagerFactory;
+
+use CrudGenerator\ConfigManager\ConfigGenerator\ManagerFactory;
+
+class GetInstanceTest extends \PHPUnit_Framework_TestCase
+{
+    public function testManagerOk()
+    {
+        chdir(__DIR__ .'/../../Data/');
+        $this->assertInstanceOf(
+            'CrudGenerator\ConfigManager\ConfigGenerator\Manager\ConfigGenerator',
+            ManagerFactory::getInstance('')
+        );
+    }
+
+    public function testManagerKo()
+    {
+        chdir(__DIR__ .'/../../Data/');
+        $this->setExpectedException(
+                'CrudGenerator\ConfigManager\ConfigGenerator\InvalidConfigPathException'
+        );
+        ManagerFactory::getInstance('toto/');
+    }
+
+    public function testManagerGetConfig()
+    {
+        chdir(__DIR__ .'/../../Data/');
+
+        $manager = ManagerFactory::getInstance('');
+
+        $this->assertInstanceOf(
+            'CrudGenerator\ConfigManager\ConfigGenerator\DataObject\ConfigDataObject',
+            $manager->getConfig()
+        );
+    }
+}
