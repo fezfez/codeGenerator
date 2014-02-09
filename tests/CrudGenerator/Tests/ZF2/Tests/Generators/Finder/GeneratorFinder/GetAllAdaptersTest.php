59b4be982aec78cef09136c7e021425f0980ac50
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2014-02-09 22:54:37 +0100

    Scrutinizer Auto-Fixes
    This patch was automatically generated as part of the following inspection:
    https://scrutinizer-ci.com/g/fezfez/codeGenerator/inspections/aa1567f9-6a0f-41f3-a694-d42b51b78cdf
    
    Enabled analysis tools:
     - PHP Analyzer
     - PHP Mess Detector
     - PHP Code Sniffer
     - PHP CS Fixer
     - JSHint
     - PHP Code Coverage
     - PHP Copy/Paste Detector
     - PHP Lines Of Code
     - PHP PDepend

--- tests/CrudGenerator/Tests/ZF2/Tests/Generators/Finder/GeneratorFinder/GetAllAdaptersTest.php
+++ tests/CrudGenerator/Tests/ZF2/Tests/Generators/Finder/GeneratorFinder/GetAllAdaptersTest.php
@@ -3,7 +3,6 @@ namespace CrudGenerator\Tests\ZF2\Tests\Generators\Finder\GeneratorFinder;
 
 use CrudGenerator\Generators\Finder\GeneratorFinder;
 use CrudGenerator\Utils\FileManager;
-use CrudGenerator\Utils\ClassAwake;
 
 class GetAllAdapatersTest extends \PHPUnit_Framework_TestCase
 {
