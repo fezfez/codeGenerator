edab826bb29b75176024b2fe47a1443a56529bf5
fezfez <demonchaux.stephane@gmail.com>
2014-02-10 08:28:40 +0100

    Add unit test

--- tests/CrudGenerator/Tests/General/Generators/GeneratorDataObject/MethodTest.php
+++ tests/CrudGenerator/Tests/General/Generators/GeneratorDataObject/MethodTest.php
@@ -2,12 +2,14 @@
 namespace CrudGenerator\Tests\General\Generators\GeneratorDataObject;
 
 use CrudGenerator\Generators\GeneratorDataObject;
+use CrudGenerator\GeneratorsEmbed\ArchitectGenerator\Architect;
 
 class MethodTest extends \PHPUnit_Framework_TestCase
 {
     public function testMethods()
     {
         $sUT = new GeneratorDataObject();
+        $sUT->setDTO(new Architect());
 
         $questions = array('dtoAttribute' => 'test');
 
@@ -22,5 +24,21 @@ class MethodTest extends \PHPUnit_Framework_TestCase
             '{"templateVariable":[],"files":[],"directories":[],"name":null,"questions":[{"dtoAttribute":"test"}],"environnement":[]}',
             json_encode($sUT)
         );
+
+        $sUT->addEnvironnementValue('backend', 'doctrine2');
+
+        $this->assertEquals(
+        	array('backend' => 'doctrine2'),
+        	$sUT->getEnvironnementCollection()
+        );
+    }
+
+    public function testFailAddEnvOnEmptyDTO()
+    {
+    	$sUT = new GeneratorDataObject();
+
+    	$this->setExpectedException('LogicException');
+
+    	$sUT->addEnvironnementValue('backend', 'doctrine2');
     }
 }
