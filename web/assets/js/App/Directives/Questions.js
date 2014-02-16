b922157feffe8328d1d6a947777e47481a6c2840
fezfez <demonchaux.stephane@gmail.com>
2014-02-17 00:39:22 +0100

    Better use of directive

--- web/assets/js/App/Directives/Questions.js
+++ web/assets/js/App/Directives/Questions.js
@@ -4,19 +4,7 @@
     GeneratorApp.directive('questions', ['$compile', '$http', function($compile, $http) {
         return {
             restrict: 'E',
-            link: function(scope, element, attrs) {
-
-                scope.$watchCollection('questionsList', function(questionsList, old) {
-                    if (angular.isObject(questionsList)) {
-                        $http.get(__BASEPATH__ + 'assets/js/App/Template/Question.html').success(function(template) {
-                            element.html(template);
-                            $compile(element.contents())(scope);
-                        });
-                    } else if(null === questionsList) {
-                        angular.element(element).hide();
-                    }
-                });
-            }
+            templateUrl: __BASEPATH__ + 'assets/js/App/Template/Question.html'
         };
     }]);
 }());
