e8992896361064c2fe9e1f8c106b208a4b58d6c9
fezfez <demonchaux.stephane@gmail.com>
2014-02-24 22:51:53 +0100

    Fix pdo config

--- tests/CrudGenerator/Tests/PDO/MetaData/PDO/PgSql/config.php
+++ tests/CrudGenerator/Tests/PDO/MetaData/PDO/PgSql/config.php
@@ -6,10 +6,9 @@ use CrudGenerator\MetaData\Sources\PDO\PDOConfig;
 $pdoConfig = new PDOConfig();
 $pdoConfig->setDatabaseName('code_generator')
 ->setType('pgsql')
-->setPassword('fezfez')
-->setUser('steph')
+->setPassword('')
+->setUser('postgres')
 ->setPort('5432')
 ->setHost('localhost');
-
 
 return $pdoConfig;
