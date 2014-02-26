b06b6f9efe00edf0d879b48614c18224b5d537b7
Stéphane Demonchaux <demonchaux.stephane@gmail.com>
2014-02-26 11:59:52 +0100

    Replace wrong type sent in ini_set

--- bin/code-generator.php
+++ bin/code-generator.php
@@ -17,7 +17,7 @@
  */
 
 
-ini_set('display_errors', true);
+ini_set('display_errors', '1');
 chdir(realpath('./'));
 
 if (!(@include_once __DIR__ . '/../vendor/autoload.php') && !(@include_once __DIR__ . '/../../../autoload.php')) {
