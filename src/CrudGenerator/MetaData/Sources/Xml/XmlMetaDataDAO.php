c59a591afffd7760784e216a5e2c51cf726a5b0a
fezfez <demonchaux.stephane@gmail.com>
2014-09-29 17:52:59 +0200

    Add xml driver

---
+++ src/CrudGenerator/MetaData/Sources/Xml/XmlMetaDataDAO.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+namespace CrudGenerator\MetaData\Sources\Xml;
+
+use CrudGenerator\MetaData\Sources\Json\JsonConfig;
+use CrudGenerator\MetaData\Sources\Json\MetadataDataObjectJson;
+use CrudGenerator\MetaData\Sources\MetaDataDAOInterface;
+use CrudGenerator\MetaData\DataObject\MetaDataCollection;
+use CrudGenerator\MetaData\DataObject\MetaDataColumnCollection;
+use CrudGenerator\MetaData\DataObject\MetaDataColumn;
+use CrudGenerator\MetaData\DataObject\MetaDataRelationCollection;
+use CrudGenerator\MetaData\DataObject\MetaDataRelationColumn;
+use JSONSchema\SchemaGenerator;
+use CrudGenerator\MetaData\Sources\Json\JsonMetaDataDAO;
+
+/**
+ * Xml adapter
+ */
+class XmlMetaDataDAO implements MetaDataDAOInterface
+{
+    /**
+     * @var JsonMetaDataDAO
+     */
+    private $jsonMetaDataDAO = null;
+
+    /**
+     * Constructor.
+     * @param JsonMetaDataDAO $jsonMetaDataDAO
+     */
+    public function __construct(JsonMetaDataDAO $jsonMetaDataDAO)
+    {
+        $this->jsonMetaDataDAO = $jsonMetaDataDAO;
+    }
+
+    /**
+     * Get all metadata from MySQL
+     *
+     * @return MetaDataCollection
+     */
+    public function getAllMetadata()
+    {
+        return $this->jsonMetaDataDAO->getAllMetadata();
+    }
+
+    /**
+     * Get particularie metadata from MySQL
+     *
+     * @param string $tableName
+     * @return \CrudGenerator\MetaData\Sources\MySQL\MetadataDataObjectMySQL
+     */
+    public function getMetadataFor($tableName, array $parentName = array())
+    {
+        return $this->jsonMetaDataDAO->getMetadataFor($tableName);
+    }
+}
---
+++ src/CrudGenerator/MetaData/Sources/Xml/XmlMetaDataDAOFactory.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+namespace CrudGenerator\MetaData\Sources\Xml;
+
+use CrudGenerator\MetaData\Sources\MetaDataDAOFactoryInterface;
+use CrudGenerator\MetaData\Sources\MetaDataConfigInterface;
+use CrudGenerator\MetaData\Sources\Json\JsonConfig;
+use CrudGenerator\MetaData\MetaDataSource;
+use CrudGenerator\MetaData\Sources\Json\JsonMetaDataDAOFactory;
+
+/**
+ * Create Xml Metadata DAO instance
+ *
+ */
+class XmlMetaDataDAOFactory implements MetaDataDAOFactoryInterface
+{
+    /**
+     * @param MetaDataConfigInterface $config
+     * @throws \InvalidArgumentException
+     * @return \CrudGenerator\MetaData\Sources\Json\JsonMetaDataDAO
+     */
+    public static function getInstance(MetaDataConfigInterface $config = null)
+    {
+        if (false === ($config instanceof XmlConfig)) {
+            throw new \InvalidArgumentException('Config must be an instance of XmlConfig');
+        }
+
+        $xml   = simplexml_load_string($config->getConnection());
+        $json  = json_encode($xml);
+
+        file_put_contents('tmp', $json);
+
+        $jsonConfig = new JsonConfig();
+        $jsonConfig->setConfigUrl('tmp');
+        $jsonMetadata = JsonMetaDataDAOFactory::getInstance($jsonConfig);
+
+        return new XmlMetaDataDAO($jsonMetadata);
+    }
+
+    /**
+     * @param MetaDataSource $metadataSource
+     * @return boolean
+     */
+    public static function checkDependencies(MetaDataSource $metadataSource)
+    {
+        return true;
+    }
+
+    /**
+     * @return \CrudGenerator\MetaData\MetaDataSource
+     */
+    public static function getDescription()
+    {
+        $dataObject = new MetaDataSource();
+        $dataObject->setDefinition("Xml")
+                   ->setMetadataDaoFactory('CrudGenerator\MetaData\Sources\Xml\XmlMetaDataDAOFactory')
+                   ->setMetadataDao("CrudGenerator\MetaData\Sources\Xml\XmlMetaDataDAO")
+                   ->setConfig(new XmlConfig());
+
+        return $dataObject;
+    }
+}
