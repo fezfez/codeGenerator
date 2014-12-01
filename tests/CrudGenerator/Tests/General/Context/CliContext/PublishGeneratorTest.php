b53cf05bc3a404c81ba6876fbbaab8e7b7f1a56e
fezfez <demonchaux.stephane@gmail.com>
2014-12-01 16:03:32 +0000

    Fix unit

--- tests/CrudGenerator/Tests/General/Context/CliContext/PublishGeneratorTest.php
+++ tests/CrudGenerator/Tests/General/Context/CliContext/PublishGeneratorTest.php
@@ -2,12 +2,13 @@
 namespace CrudGenerator\Tests\General\Context\CliContext;
 
 use CrudGenerator\Context\CliContext;
-use CrudGenerator\Tests\TestCase;
+use CrudGenerator\DataObject;
 use CrudGenerator\Generators\GeneratorDataObject;
+use CrudGenerator\Tests\TestCase;
 
 class PublishGeneratorTest extends TestCase
 {
-    public function testLogIsCorrectlyCall()
+    public function testPublishingFailWithoutDto()
     {
         $outputStub        = $this->createMock('Symfony\Component\Console\Output\ConsoleOutput');
         $inputStub         = $this->createMock('Symfony\Component\Console\Input\ArrayInput');
@@ -16,6 +17,23 @@ class PublishGeneratorTest extends TestCase
 
         $sUT = new CliContext($questionHelper, $outputStub, $inputStub, $createCommandMock);
 
-        $this->assertEmpty($sUT->publishGenerator(new GeneratorDataObject()));
+        $this->setExpectedException('Exception');
+
+        $sUT->publishGenerator(new GeneratorDataObject());
+    }
+
+    public function testPublishingOk()
+    {
+        $outputStub        = $this->createMock('Symfony\Component\Console\Output\ConsoleOutput');
+        $inputStub         = $this->createMock('Symfony\Component\Console\Input\ArrayInput');
+        $questionHelper    = $this->createMock('Symfony\Component\Console\Helper\QuestionHelper');
+        $createCommandMock = $this->createMock('CrudGenerator\Command\CreateCommand');
+
+        $sUT = new CliContext($questionHelper, $outputStub, $inputStub, $createCommandMock);
+
+        $generatorDto = new GeneratorDataObject();
+        $generatorDto->setDto(new DataObject());
+
+        $this->assertEmpty($sUT->publishGenerator($generatorDto));
     }
 }
