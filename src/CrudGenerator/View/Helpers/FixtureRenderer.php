4cfb9b7a91de479f28620e1f8e258cb5225d5ba1
fezfez <demonchaux.stephane@gmail.com>
2014-12-03 10:07:22 +0000

    Clean

--- src/CrudGenerator/View/Helpers/FixtureRenderer.php
+++ src/CrudGenerator/View/Helpers/FixtureRenderer.php
@@ -34,19 +34,68 @@ class FixtureRenderer
     public function render(MetaDataColumn $metadata)
     {
         $data = '';
-        if (in_array($metadata->getType(), array('integer', 'float')) === true) {
+
+        if ($this->isNumber($metadata->getType()) === true) {
             $data = $this->faker->randomNumber();
-        } elseif (in_array($metadata->getType(), array('string', 'text')) === true) {
-            $data = ($metadata->getLength() === null) ?
-                        '"' . $this->faker->text(50) . '"' :
-                        '"' . ($metadata->getLength() <=  5) ?
-                            '"test"' : $this->faker->text($metadata->getLength()) . '"';
-        } elseif ($metadata->getType() === 'date') {
+        } elseif ($this->isString($metadata->getType()) === true) {
+            $data = $this->generateString($metadata);
+        } elseif ($this->isDate($metadata->getType()) === true) {
             $data = 'new DateTime()';
-        } elseif (in_array($metadata->getType(), array('bool', 'boolean')) === true) {
+        } elseif ($this->isBool($metadata->getType()) === true) {
             $data = 'true';
         }
 
         return $data;
+    }
+
+    /**
+     * @param MetaDataColumn $metadata
+     * @return string
+     */
+    private function generateString(MetaDataColumn $metadata)
+    {
+        if ($metadata->getLength() === null) { ?
+            return '"' . $this->faker->text(50) . '"';
+        } elseif($metadata->getLength() <=  5) {
+            return 'test';
+        } else {
+            return $this->faker->text($metadata->getLength()) . '"';
+        }
+    }
+
+    /**
+     * @param string $type
+     * @return boolean
+     */
+    private function isString($type)
+    {
+        return in_array($type, array('string', 'text'));
+    }
+
+    /**
+     * @param string $type
+     * @return boolean
+     */
+    private function isNumber($type)
+    {
+        return in_array($type, array('integer', 'float'));
+    }
+
+    /**
+     * @param string $type
+     * @return boolean
+     */
+    private function isBool($type)
+    {
+        return in_array($type, array('bool', 'boolean'));
+    }
+
+    /**
+     * @param string $type
+     * @return boolean
+     */
+    private function isDate($type)
+    {
+        return $type === 'date';
     }
 }
