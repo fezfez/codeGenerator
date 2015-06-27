aba9d87254997994fe65e2de1d22212503d5d959
fezfez <demonchaux.stephane@gmail.com>
2015-06-27 21:26:13 +0200

    Fix use of deprecated method

--- tests/CrudGenerator/Tests/General/Generators/Parser/Lexical/FileParser/EvaluateTest.php
+++ tests/CrudGenerator/Tests/General/Generators/Parser/Lexical/FileParser/EvaluateTest.php
@@ -148,7 +148,7 @@ class EvaluateTest extends TestCase
         $fileManager        = $this->createMock('CrudGenerator\Utils\FileManager');
         $conditionValidator = $this->createMock('CrudGenerator\Generators\Parser\Lexical\Condition\ConditionValidator');
         $phpParser          = new PhpStringParser(
-            new \Twig_Environment(new \Twig_Loader_Array(array()), array('test' => 'well')
+            new \Twig_Environment(new \Twig_Loader_Array(array())), array('test' => 'well')
         );
 
         $conditionValidator->expects($this->once())
